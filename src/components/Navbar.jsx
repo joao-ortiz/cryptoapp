a23b90f5c88e6b98b096bc785792d0a29eb42500
João Ortiz <smurfjooj@gmail.com>
2022-01-09 20:15:23 +0000

    gh-pages deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,16 +41,16 @@ const Navbar = () => {
         {activeMenu && (
         <Menu theme="dark">
             <Menu.Item key="Home" icon={<HomeOutlined />}>
-            <Link to="/">Home</Link>
+                <Link to="/">Home</Link>
             </Menu.Item>
             <Menu.Item key="Cryptos" icon={<FundOutlined />}>
-            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
             </Menu.Item>
             {/* <Menu.Item icon={<MoneyCollectOutlined />}>
             <Link to="/exchanges">Exchanges</Link>
             </Menu.Item> */}
             <Menu.Item key="News" icon={<BulbOutlined />}>
-            <Link to="/news">News</Link>
+                <Link to="/news">News</Link>
             </Menu.Item>
         </Menu>
         )}
